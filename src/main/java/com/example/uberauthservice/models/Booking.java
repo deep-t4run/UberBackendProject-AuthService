5dea908fc4cc415ebaa4331612f989485666ec2d
deep-t4run <deep.t4run@gmail.com>
2025-06-28 12:36:37 +0530

    Added a new migration to add email and phone number and copied migrations from review service

---
+++ src/main/java/com/example/uberauthservice/models/Booking.java
@@ -0,0 +1,32 @@
+package com.example.uberauthservice.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel{
+
+    @Enumerated(value = EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+
+    @ManyToOne
+    private Driver driver;
+
+    @ManyToOne
+    private Passenger passenger;
+}
---
+++ src/main/java/com/example/uberauthservice/services/AuthService.java
@@ -0,0 +1,7 @@
+package com.example.uberauthservice.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+}
